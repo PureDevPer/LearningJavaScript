d8b4296d2b57dc0a21cd57812eeedff094447a0c
PureDevPer <wooseok.kim1207@gmail.com>
2019-03-11 01:06:16 -0400

    cloning Momentum

--- Momentum/todo.js
+++ Momentum/todo.js
@@ -3,15 +3,52 @@ const toDoInput = toDoForm.querySelector("input"),
     toDoList = document.querySelector(".js-toDoList");
 const TODOS_LS = 'toDos';
 
+let toDos = [];
+
+
+function deleteToDo(event){
+    //console.log(event.target.parentNode);
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+        //console.log(toDo.id, parseInt(li.id));
+        return toDo.id !== parseInt(li.id);
+    });
+    //console.log(cleanToDos);
+    toDos = cleanToDos;
+    saveToDos();
+}
+
+
+
+function saveToDos(){
+    // JSON.stringify: Convert Object -> String
+    // Because JavaScript saves toDos as an object
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+
 function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerText = "❌";
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
+
+    delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
+    
     span.innerText = text;
     li.appendChild(span);
     li.appendChild(delBtn);
+    li.id = newId;
     toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 
@@ -22,11 +59,24 @@ function handleSubmit(event){
     toDoInput.value = "";
 }
 
+/*
+function something (toDo){
+    console.log(toDo.text);
+}
+*/
 
 function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null){
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null){
+        //console.log(loadedToDos);
+        const parsedToDos = JSON.parse(loadedToDos);
+        //console.log(parsedToDos);
 
+        // Show the list of li using forEach
+        //parsedToDos.forEach(something);
+        parsedToDos.forEach(function (toDo){
+            paintToDo(toDo.text);
+        });
     } 
 }
 
